88390f62fc01b7e4a2f07bb1055b6cdfff2bf046
BGdaredevil <dimityr.dimitr0v@abv.bg>
2021-11-07 10:47:50 +0200

    add base structure and cleanup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { Switch, Route, Redirect } from "react-router-dom";
+
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      {/* <ErrorContextProvider> */}
+      {/* <AuthContextProvider> */}
+      {/* <ErrorComp /> */}
+      {/* <Navigation /> */}
+      {/* <Switch> */}
+      {/* <Route exact path="/" component={Home} /> */}
+      {/* <Route path="*" component={Error404} /> */}
+      {/* </Switch> */}
+      {/* <Footer /> */}
+      {/* </AuthContextProvider> */}
+      {/* </ErrorContextProvider> */}
+      Hello
+    </div>
+  );
 }
 
 export default App;
