80d085bfb0098e77b134dfe174c1ec515739b449
BGdaredevil <dimityr.dimitr0v@abv.bg>
2021-11-08 15:57:34 +0200

    footer stuff

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,42 @@
+import styles from "./Footer.module.css";
+
 function Footer() {
   return (
-    <div className="footer">
-      <h1>DD &copy;</h1>
-    </div>
+    <footer className={styles.footerContainer}>
+      <div className={styles.logoSection}>
+        <section>WIP LOGO</section>
+        <section>company details</section>
+      </div>
+      <div className={styles.infoSection}>
+        <section>
+          <i>icon</i>
+          <p>address</p>
+        </section>
+        <section>
+          <i>icon</i>
+          <p>phone</p>
+        </section>
+        <section>
+          <i>icon</i>
+          <p>email</p>
+        </section>
+      </div>
+      <div className={styles.aboutSection}>
+        <section>
+          <p>about</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis autem iusto
+            suscipit culpa laudantium repellendus incidunt accusantium adipisci aperiam odio?
+          </p>
+          <div>
+            <i>facebook</i>
+            <i>instagram</i>
+            <i>linkedin</i>
+            <i>github</i>
+          </div>
+        </section>
+      </div>
+    </footer>
   );
 }
 
